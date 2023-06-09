8dfadbf0b8da0cfa72370ac3afcc2089ec302479
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2023-06-09 15:56:44 +0700

    added GameComponentNew as an abstract class

--- src/game-component/GameComponent.ts
+++ src/game-component/GameComponent.ts
@@ -1,5 +1,44 @@
 import { GameObject } from '../game-object/GameObject'
 
+export abstract class GameComponentNew {
+    protected gameObject: GameObject | undefined
+    protected isDestroyed: boolean
+    protected onStart: (() => void) | undefined
+    protected onUpdate: ((delta: number) => void) | undefined
+    protected onDestroy: (() => void) | undefined
+
+    constructor() {
+        this.isDestroyed = false
+    }
+
+    public abstract getType(): GameComponentType
+    public getGameObject(): GameObject | undefined {
+        return this.gameObject
+    }
+    public setGameObject(value: GameObject | undefined): void {
+        this.gameObject = value
+    }
+    public init(gameObject: GameObject): void {
+        this.gameObject = gameObject
+
+        // TODO: subscribe start, update to game events
+    }
+    private start(): void {
+        this.onStart?.()
+    }
+    private update(delta: number): void {
+        this.onUpdate?.(delta)
+    }
+    public destroy(): void {
+        if (this.isDestroyed) {
+            throw new Error('Cannot destroy a destroyed game component')
+        }
+
+        this.isDestroyed = true
+
+        this.onDestroy?.()
+    }
+}
 export abstract class GameComponent {
     public abstract get type(): GameComponentType
     public abstract get gameObject(): GameObject | undefined
