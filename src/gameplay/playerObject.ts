4fb602b0dd4d6c9a2a358a43490a08f2ab21c188
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2023-06-12 11:15:01 +0700

    fixes gameplay's player object

--- src/gameplay/playerObject.ts
+++ src/gameplay/playerObject.ts
@@ -2,15 +2,14 @@ import { RectangleCollider } from '../collider/RectangleCollider'
 import { canvasSize } from '../canvasSize'
 import { Player } from '../player/Player'
 import { SpriteRenderer } from '../sprite-renderer/SpriteRenderer'
-import { Transform } from '../transform/Transform'
 
 // player collider
 const playerCollider = new RectangleCollider({
     tag: 'Player',
-    x: 0,
-    y: 0,
-    width: 50,
-    height: 50,
+    position: {x: 0,
+    y: 0},
+    size: {x: 50,
+    y: 50}
 })
 
 // player sprite
@@ -31,11 +30,10 @@ const player = new Player({
     speed: 80,
     gravity: 2,
     friction: 1,
-    groundCollider: playerCollider,
 })
 
 // player's transform
-const playerTransform = player.getComponent('Transform') as Transform
+const playerTransform = player.getTranform()
 
 if (!playerTransform) {
     throw new Error('Player must have a transform')
